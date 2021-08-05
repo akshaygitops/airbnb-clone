b584855c3689d1091cb75571e08b031fef00a1e2
Akshay <akshaykhandelwal79@gmail.com>
2021-08-05 21:32:23 +0530

    intial commit, added hero, main and footer section

---
+++ components/Footer.js
@@ -0,0 +1,45 @@
+function Footer() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">ABOUT</h5>
+        <p>How Airbnb works</p>
+        <p>Newsroom</p>
+        <p>Investor</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+        <p>HotelTonight</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">COMMUNITY</h5>
+        <p>Accessibility</p>
+        <p>This is not a real site</p>
+        <p>Its a pretty awesome clone</p>
+        <p>Diversity & Belonging</p>
+        <p>Referrals</p>
+        <p>Airbnbclone.org</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">HOST</h5>
+        <p>How your home</p>
+        <p>Host an Online Experience</p>
+        <p>Host an Experience</p>
+        <p>Responsible hosting</p>
+        <p>Resource Center</p>
+        <p>Community Center</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">SUPPORT</h5>
+        <p>Help Center</p>
+        <p>Trust & Safety</p>
+        <p>Cancellation options</p>
+        <p>Neighbourhood Support</p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
